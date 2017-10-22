e1c39d55fd853e4a2e6bd3d4597db0ef8b2b5a8f
John <601473400@qq.com>
2017-10-22 23:13:14 +0800

    first commit

---
+++ practice_flask/config.py
@@ -0,0 +1,45 @@
+import os
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True  # 设置True, 每次请求结束后会提交数据库的变动
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # 如果设置成 True (默认情况)，Flask-SQLAlchemy 将会追踪对象的修改并且发送信号
+    MAIL_PORT = 25
+    MAIL_SERVER = 'smtp.qq.com'
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')  # 密码要设置成第三方授权码
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'  # 邮件标题前缀
+    FLASKY_MAIL_SENDER = os.environ.get('MAIL_USERNAME')
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'mysql://root:19910826@localhost/data_dev'
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'mysql://root:19910826@localhost/data_test'
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'mysql://root:19910826@localhost/data_prd'
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
